c9d8f9245ff073c8586a61fac352e085b59f3ed3
Cathal Burke <g00413483@atu.ie>
2025-10-07 10:27:19 +0100

    Initial commit for the CI CD Library System project for our third year CI/CD project setting up a basic Spring Boot project using Maven in IntelliJ

---
+++ src/main/java/ie/atu/ci_cd_library_system/CiCdLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.ci_cd_library_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdLibrarySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdLibrarySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/ci_cd_library_system/CiCdLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.ci_cd_library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdLibrarySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
