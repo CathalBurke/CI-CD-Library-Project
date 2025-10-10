b1fc10ab94637d6daae2199592e90215a8998775
Patrick Flanagan <G00431255@atu.ie>
2025-10-10 11:33:41 +0100

    Post request used to create new user and add it to array list

--- src/main/java/ie/atu/ci_cd_library_system/LoginController.java
+++ src/main/java/ie/atu/ci_cd_library_system/LoginController.java
@@ -2,6 +2,7 @@ package ie.atu.ci_cd_library_system;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,12 +10,12 @@ import java.util.List;
 @RestController
 public class LoginController {
     //hardcoded list of admin users no permissions yet just a login
-    private final List<Login> users= Arrays.asList(
+    private final List<Login> users=new ArrayList<>(Arrays.asList(
         new Login("patrick","password") ,
         new Login("cathal","password")   ,
         new Login("fionn","password")  ,
         new Login("enzo","password")
-    );
+    ));
     /*@GetMapping("/add")
     public  LoginResponse addUser(@RequestParam String username, @RequestParam String password){
         users.add
@@ -31,7 +32,16 @@ public class LoginController {
     }
 
 
-
+        @PostMapping("/users")//create
+        public LoginResponse createUser(@RequestBody Login Newuser){
+        for(int i=0;i<users.size();i++){
+            if(users.get(i).getUsername().equals(Newuser.getUsername())){
+                return new LoginResponse("User already exists!");
+            }
+        }
+        users.add(Newuser);
+        return new LoginResponse("User has been created! Welcome "+Newuser.getUsername());
+        }
     @GetMapping("/list")//read
     public List<Login> getUsers(){
         return users;
