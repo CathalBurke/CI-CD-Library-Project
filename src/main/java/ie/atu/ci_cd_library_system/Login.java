55feb8ff056614cf27d907e92f9a659b9d7f0ec1
Cathal Burke <g00413483@atu.ie>
2025-11-03 12:22:26 +0000

    added validation to the user, login and their controllers also updated the pom and my project to java 21

--- src/main/java/ie/atu/ci_cd_library_system/Login.java
+++ src/main/java/ie/atu/ci_cd_library_system/Login.java
@@ -4,14 +4,18 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Login {
-    @NotBlank
-    @Size(min = 3, max = 20)
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
-    @NotBlank
-    @Size(min = 3, max = 20)
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 3, max = 20, message = "Password must be between 3 and 20 characters")
     private String password;
 }
--- src/main/java/ie/atu/ci_cd_library_system/User.java
+++ src/main/java/ie/atu/ci_cd_library_system/User.java
@@ -1,8 +1,17 @@
 package ie.atu.ci_cd_library_system;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
 
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please enter a valid email address")
     private String email;
 
     public User() {
