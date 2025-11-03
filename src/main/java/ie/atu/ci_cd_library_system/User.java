a9bb13350db43de8b07cad5876c3f749df4363be
Cathal Burke <g00413483@atu.ie>
2025-11-03 12:27:07 +0000

    changed the getters and setters to ALLArgs constructor

--- src/main/java/ie/atu/ci_cd_library_system/User.java
+++ src/main/java/ie/atu/ci_cd_library_system/User.java
@@ -3,7 +3,13 @@ package ie.atu.ci_cd_library_system;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @NotBlank(message = "Name cannot be blank")
@@ -13,28 +19,5 @@ public class User {
     @NotBlank(message = "Email is required")
     @Email(message = "Please enter a valid email address")
     private String email;
-
-    public User() {
-    }
-
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
+
