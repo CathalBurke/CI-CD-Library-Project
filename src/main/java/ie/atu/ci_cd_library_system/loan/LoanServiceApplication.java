b070ff32033dacfa179727a1754c47e5b81971c0
FIONNOROURKE-STUDENT <G00429672@atu.ie>
2025-12-12 10:52:17 +0000

    not fully working

--- src/main/java/ie/atu/ci_cd_library_system/loan/LoanServiceApplication.java
+++ src/main/java/ie/atu/ci_cd_library_system/loan/LoanServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class LoanServiceApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(LoanServiceApplication.class, args);
     }
 }
